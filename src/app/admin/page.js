91df8728605272681faaf471873531f826aac31a
henry21209 <adcpapa@gmail.com>
2026-01-16 04:35:49 +0800

    admin modify

--- src/app/admin/page.js
+++ src/app/admin/page.js
@@ -1,6 +1,6 @@
 "use client";
 import { useState, useEffect } from "react";
-import { db } from "@/firebase";
+import { db } from "@/app/firebase";
 import { collection, getDocs, addDoc, deleteDoc, updateDoc, doc, query, where, writeBatch } from "firebase/firestore";
 import { getAuth, signInWithPopup, GoogleAuthProvider, signOut, onAuthStateChanged } from "firebase/auth";
 
